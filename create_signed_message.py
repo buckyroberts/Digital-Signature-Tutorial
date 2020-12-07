0f52d4d02d15dfad274e2baaaca6abfa8dbd77a3
Bucky <buckyroberts@gmail.com>
2020-12-06 19:19:34 -0800

    Update

--- create_signed_message.py
+++ create_signed_message.py
@@ -4,7 +4,7 @@ from config import SIGNED_MESSAGE_FILE, SIGNING_KEY_FILE
 from utils import generate_signed_message, write_json
 
 SIGNING_KEY = read_signing_key_file(SIGNING_KEY_FILE)
-MESSAGE = 'bacon'
+MESSAGE = 'My name is Bucky Roberts'
 
 account_number = SIGNING_KEY.verify_key
 signed_message = generate_signed_message(
